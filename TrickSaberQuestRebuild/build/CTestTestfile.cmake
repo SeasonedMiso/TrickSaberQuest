2f7284842ed00c8119e8173356e0e70a07135b38
Skye Wren <swren@usonar.co.jp>
2025-08-06 10:52:52 +0900

    remove build files

--- TrickSaberQuestRebuild/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/mac83/dev/TrickSaber/TrickSaberQuestRebuild
-# Build directory: /Users/mac83/dev/TrickSaber/TrickSaberQuestRebuild/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/mac83/dev/TrickSaber/TrickSaberQuestRebuild/build/tricksaber_test[1]_include.cmake")
-subdirs("_deps/googletest-build")
